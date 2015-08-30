d524b7f94ff5a567379e1926bba6bc3ccb9ba444
Eric Streske <estreske@gmail.com>
2015-08-30 16:20:12 -0400

    changed body input name

--- php/send_email.php
+++ php/send_email.php
@@ -2,16 +2,16 @@
 
 $email = $_GET['email'];
 $name = $_GET['name'];
-$subject = $_GET['subject'];
-$body = $_GET['body'];
+$from_subject = $_GET['subject'];
+$from_body = $_GET['body'];
 
 $to      = 'estreske@gmail.com';
 $subject = 'New Contact Form Submission from Streske.com';
 $message = "You have received a new contact submission from Streske.com containing the following info:
 Email: " . $email . "
 Name: " . $name . "
-Subject: " . $subject . "
-Body: " . $body;
+Subject: " . $from_subject . "
+Body: " . $from_body;
 $headers = 'From: contact@streske.com' . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
 
