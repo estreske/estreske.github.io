a9f5ef526cb073d6c7dce8c2629011fe7c0a4591
Eric Streske <estreske@gmail.com>
2015-08-30 16:13:22 -0400

    changed email maessage format

--- php/send_email.php
+++ php/send_email.php
@@ -7,14 +7,14 @@ $body = $_GET['body'];
 
 $to      = 'estreske@gmail.com';
 $subject = 'New Contact Form Submission from Streske.com';
-$message = 'You have received a new contact submission from Streske.com containing the following info:\n\nEmail: {$email}\n\nName: {$name}\n\nSubject: {$subject}\n\nBody: {$body}\n\n';
+$message = "You have received a new contact submission from Streske.com containing the following info:<br><br>Email: " . $email . "<br><br>Name: " . $name . "<br><br>Subject: " . $subject . "<br><br>Body: " . $body;
 $headers = 'From: contact@streske.com' . "\r\n" .
             'X-Mailer: PHP/' . phpversion();
 
 $mail = mail($to, $subject, $message, $headers);
 
 if (!$mail) {
-  echo "Mail COuld not be sent";
+  echo "Mail Could not be sent";
 }
 
 
